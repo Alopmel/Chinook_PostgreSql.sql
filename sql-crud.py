ee76bf0eff98e2145138c7c441affa9007f3e046
Alopmel <alba-lopez_student2021@wilder.school>
2022-02-23 12:02:17 +0000

    Trying to update and delete records

--- sql-crud.py
+++ sql-crud.py
@@ -80,18 +80,60 @@ tim_berners_lee = Programmer(
     famous_for="World Wide Web"
 )
 
+alba_lopez = Programmer(
+    first_name="Alba",
+    last_name="López",
+    gender="F",
+    nationality="Spanish",
+    famous_for="Programmer"
+)
+
 # add each instance of our programmers to our session
-session.add(ada_lovelace)
-session.add(alan_turing)
-session.add(grace_hopper)
-session.add(margaret_hamilton)
-session.add(bill_gates)
-session.add(tim_berners_lee)
+# session.add(ada_lovelace)
+# session.add(alan_turing)
+# session.add(grace_hopper)
+# session.add(margaret_hamilton)
+# session.add(bill_gates)
+# session.add(tim_berners_lee)
+# session.add(alba_lopez)
+
+
+# updating a single record
+# programmer = session.query(Programmer).filter_by(id=7).first()
+# programmer.famous_for = "World President"
+
 
 # commit our session to the database
-session.commit()
+# session.commit()
+
+# updating multiple records
+# people = session.query(Programmer)
+# for person in people:
+#     if person.gender == "F":
+#         person.gender = "Female"
+#     elif person.gender == "M":
+#         person.gender = "Male"
+#     else:
+#         print("Gender not define")
+#     session.commit()
 
 
+# deleting a single record
+fname = input("Enter a first name: ")
+lname = input("Enter a last name: ")
+programmer = session.query(Programmer).filter_by(first_name=fname, last_name=lname).first()
+# defensive programming
+if programmer is not None:
+    print("Programmer found: ", programmer.first_name + " " + programmer.last_name)
+    confirmation = input("Are you sure you want to delete? (y/n)")
+    if confirmation.lower() == "y":
+        session.delete(programmer)
+        session.commit()
+        print("Programmmer has been deleted")
+    else: 
+        print("Programmer not deleted")
+else:
+    print("No records found")
 # query the database to find all Programmers
 programmers = session.query(Programmer)
 for programmer in programmers:
